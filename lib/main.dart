b04256dbcfd53cd9652df2b9fc86c8414707ded4
winahyursn <winahyu_rsn@yahoo.co.id>
2023-09-30 15:26:45 +0700

    Perubahan ojan ojan

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo Belajar Laikkkkkkkkk',
+      title: 'Flutter Demo Belajar ojan ojan',
       theme: ThemeData(
         // This is the theme of your application.
         //
